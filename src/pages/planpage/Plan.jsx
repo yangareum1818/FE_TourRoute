9c239a2a316d5a14daba33afd2a9049ef95e0c05
lovaoi777 <lovaoi777@naver.com>
2023-08-23 06:46:44 +0900

    feat : edit route

--- src/pages/planpage/Plan.jsx
+++ src/pages/planpage/Plan.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import PlanProgress from './ planprogress/PlanProgress';
+import PlanProgress from './planprogress/PlanProgress';
 import { Outlet } from 'react-router-dom';
 
 const Wrapper = styled.div`
