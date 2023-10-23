74ddff67c813a9b1dae1606b5550cb4597af3916
areumYang <yangareum1818@nate.com>
2023-10-23 23:41:26 +0900

    feat : 내 정보 수정 + css 디자인변경

--- src/pages/mypage/ProfileManagementContainer.jsx
+++ src/pages/mypage/ProfileManagementContainer.jsx
@@ -1,8 +1,8 @@
-import { Button, ButtonGroup } from 'components/common/Button';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { axiosTokenGet } from 'utils/AxiosUtils';
+import { useCallback, useEffect, useState } from 'react';
+import { axiosTokenGet, axiosTokenPut } from 'utils/AxiosUtils';
+import { Button, ButtonGroup } from 'components/common/Button';
+import { useNavigate } from 'react-router-dom';
 
 const MyProfileContent = styled.div`
 	display: flex;
@@ -14,11 +14,9 @@ const MyProfileContent = styled.div`
 `;
 
 const ProfileInfoWrpper = styled.div`
-	position: relative;
 	display: flex;
 	align-items: center;
-
-	& > * {
+	gap & > * {
 		font-size: 1.6rem;
 	}
 `;
@@ -29,7 +27,7 @@ const ProfileInfoTitle = styled.label`
 	white-space: nowrap;
 `;
 const ProfileInfoValue = styled.input`
-	flex: 4;
+	flex: 3;
 	padding: 0 2rem;
 	height: 4rem;
 	border: 0.1rem solid #cfcfcf;
@@ -39,7 +37,7 @@ const ProfileInfoValue = styled.input`
 `;
 
 const ProfileInfoValueText = styled.span`
-	flex: 4.4;
+	flex: 3.4;
 	color: #959696;
 	font-weight: 300;
 `;
@@ -50,73 +48,75 @@ const ProfileInfoChangeText = styled.span`
 	font-weight: 300;
 `;
 
-const ProfileChangeBtn = styled.button`
-	position: absolute;
-	right: 2rem;
-	top: 1.2rem;
-	color: #3ad0ff;
-	font-size: 1.6rem;
-	font-weight: 700;
-`;
-
 const ProfileManagementContainer = () => {
+	const navigate = useNavigate();
 	const [user, setUser] = useState({
 		username: '',
 		email: '',
 		latest: '',
 	});
-	const [disable, setDisable] = useState(false);
 	const { username, email, latest } = user;
-	const LastChangeDate = latest.replace('T', ' ').split('.', 1);
-
-	const ref = useRef(null);
-	// const Info = useSelector(state => state.Info);
-	// const dispatch = useDispatch();
-
-	const ProfileValueChange = () => {
-		console.log(ref.current.value);
-		return <ProfileInfoValue disable={() => setDisable(true)} />;
-	};
-
-	const onChange = e => {
-		const name = e.currentTarget.name;
-		const values = e.currentTarget.value;
-
-		console.log(name, values);
-	};
+	let LastChangeDate = latest.replace('T', ' ').split('.', 1).join('');
 
 	const userInfo = useCallback(async () => {
-		const res = await axiosTokenGet('/users/mypage');
-		// console.log('res', res, Info);
+		const data = await axiosTokenGet('/users/mypage');
+		setUser(data);
+	}, []);
 
-		setUser(res);
-	}, [setUser]);
+	const onChange = e => {
+		const { value, name } = e.target;
+
+		setUser({
+			...user,
+			[name]: value,
+		});
+	};
+
+	const onUpdateProfile = useCallback(async () => {
+		const updateData = await axiosTokenPut('/users/update_mypage');
+		setUser(updateData);
+
+		console.log(updateData);
+	}, []);
 
 	useEffect(() => {
 		userInfo();
-	}, [userInfo]);
+	}, []);
 
 	return (
 		<>
 			<MyProfileContent>
-				<ProfileInfoWrpper>
-					<ProfileInfoTitle>이름</ProfileInfoTitle>
-					<ProfileInfoValue onChange={onChange} value={username} />
-					<ProfileChangeBtn onClick={ProfileValueChange}>수정</ProfileChangeBtn>
-				</ProfileInfoWrpper>
-				<ProfileInfoWrpper>
-					<ProfileInfoTitle>이메일</ProfileInfoTitle>
-					<ProfileInfoValue ref={ref} defaultValue={email} />
-					<ProfileChangeBtn onClick={ProfileValueChange}>수정</ProfileChangeBtn>
-				</ProfileInfoWrpper>
-				<ProfileInfoWrpper>
-					<ProfileInfoTitle>마케팅 수신동의</ProfileInfoTitle>
-					<ProfileInfoValueText>수신거부</ProfileInfoValueText>
-				</ProfileInfoWrpper>
-				<ProfileInfoChangeText>최근 수정일 : {LastChangeDate[0]}</ProfileInfoChangeText>
+				<div style={{ display: 'flex', gap: '2rem' }}>
+					<div style={{ flex: 1 }}>
+						<label htmlFor="ex_file">
+							<div className="btn_select">이미지 선택</div>
+						</label>
+						<input type="file" id="ex_file" accept="image/*" name="profile_img" />
+					</div>
+					<div style={{ flex: 4 }}>
+						<ProfileInfoWrpper>
+							<ProfileInfoTitle>이름</ProfileInfoTitle>
+							<ProfileInfoValue
+								name="username"
+								defaultValue={username}
+								required
+								onChange={onChange}
+							/>
+						</ProfileInfoWrpper>
+						<ProfileInfoWrpper>
+							<ProfileInfoTitle>이메일</ProfileInfoTitle>
+							<ProfileInfoValue name="email" defaultValue={email} onChange={onChange} />
+						</ProfileInfoWrpper>
+						<ProfileInfoWrpper>
+							<ProfileInfoTitle>마케팅 수신동의</ProfileInfoTitle>
+							<ProfileInfoValueText>수신거부</ProfileInfoValueText>
+						</ProfileInfoWrpper>
+					</div>
+				</div>
+				<ProfileInfoChangeText>최근 수정일 : {LastChangeDate}</ProfileInfoChangeText>
 			</MyProfileContent>
 			<ButtonGroup style={{ maxWidth: '40rem', margin: '0 auto', paddingTop: '4rem' }}>
-				<Button text="프로필 수정 완료" />
+				<Button text="프로필 수정 완료" $submit onClick={onUpdateProfile} />
 				<Button text="취소" variant="cancel" />
 			</ButtonGroup>
 		</>
