ee157f904aa7024bd496530887fea24a52799147
lovaoi777 <lovaoi777@naver.com>
2023-10-25 16:10:19 +0900

    fix : redux-toolkit  state 유지

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from 'store/store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const persistor = persistStore(store); //새로 고침, 종료해도 지속될 store 생성
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 );
